6e062b63bbaf7dd2877203d784a34ed87dfbb2ce
Evan <evanrulfs@gmail.com>
2014-03-27 15:17:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opextracker::Application.config.secret_key_base = '9a72c5cacd03cd2d5c49eb3cf5b5c283d38038c010b661991819849b0ed8a2d7153c97a246a5ae4c090bddfcac382c2a19de7e3b7b66b11ea19421c7240a497b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Opextracker::Application.config.session_store :cookie_store, key: '_opextracker_session'
